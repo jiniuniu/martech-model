6286e098e3b30f5a9272985439dcad5b2e87933f
jiniuniu <nianjiniuniu@gmail.com>
2024-11-26 17:09:45 +0800

    add audio input

--- vocal-glass/cl_app.py
+++ vocal-glass/cl_app.py
@@ -1,37 +1,72 @@
+from io import BytesIO
+from typing import List
+
 import chainlit as cl
 from assistants.chains import build_vision_chat_chain
 from assistants.llms import get_llm
 from assistants.sys_prompts import DEFAULT
+from audio.api import transcribe_audio
+from chainlit.element import ElementBased
 from PIL import Image
 
 
-def resize_image(image_path, max_size=1024):
+def resize_image(image_path: str, max_size=512):
+
     with Image.open(image_path) as img:
-        img.thumbnail(
-            (max_size, max_size)
-        )  # Resizes the image while maintaining aspect ratio
-        resized_path = (
-            f"{image_path.rsplit('.', 1)[0]}_resized.{image_path.rsplit('.', 1)[-1]}"
-        )
-        img.save(resized_path, quality=85)  # Save with reduced quality if necessary
-        return resized_path
+        img.thumbnail((max_size, max_size))
+        img.save(image_path, quality=85)  # Save with reduced quality if necessary
 
 
-@cl.on_message
-async def chat(message: cl.Message):
-    images = [file for file in message.elements if "image" in file.mime]
-    session_id = cl.user_session.get("id")
-    msg = cl.Message(content="")
-
+async def process_images(elements: List[ElementBased]) -> List[str]:
     img_paths = []
-    if images:
-        img_paths = [resize_image(image.path, max_size=1024) for image in images]
+    for element in elements:
+        if "image" in element.mime:
+            img_paths.append(resize_image(element.path))
+    return img_paths
+
+
+async def handle_user_input(
+    user_input: str,
+    img_paths: List[str],
+    session_id: str,
+):
     chain = build_vision_chat_chain(get_llm(), DEFAULT, img_paths)
-    inp = {"user_input": message.content}
+    inp = {"user_input": user_input}
+    msg = cl.Message(content="")
     async for chunk in chain.astream(
         inp, config={"configurable": {"session_id": session_id}}
     ):
         if chunk:
             await msg.stream_token(chunk)
-
     await msg.update()
+
+
+@cl.on_message
+async def chat(message: cl.Message):
+    session_id = cl.user_session.get("id")
+    images = [file for file in message.elements if "image" in file.mime]
+    img_paths = await process_images(images)
+    await handle_user_input(message.content, img_paths, session_id)
+
+
+@cl.on_audio_chunk
+async def audio_input(chunk: cl.AudioChunk):
+    if chunk.isStart:
+        buffer = BytesIO()
+        cl.user_session.set("audio_buffer", buffer)
+    cl.user_session.get("audio_buffer").write(chunk.data)
+
+
+@cl.on_audio_end
+async def on_audio_end(elements: list[ElementBased]):
+    # Get the audio buffer from the session
+    audio_buffer: BytesIO = cl.user_session.get("audio_buffer")
+    user_input = transcribe_audio(audio_buffer)
+    await cl.Message(
+        author="You",
+        type="user_message",
+        content=user_input,
+    ).send()
+    session_id = cl.user_session.get("id")
+    img_paths = await process_images(elements)
+    await handle_user_input(user_input, img_paths, session_id)
