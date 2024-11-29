4f1e52274922f05f1c977703c9db7739c83c99df
jiniuniu <nianjiniuniu@gmail.com>
2024-11-29 15:43:18 +0800

    mount on fastapi

---
+++ vocal-glass/app.py
@@ -0,0 +1,16 @@
+from chainlit.utils import mount_chainlit
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+mount_chainlit(app, "cl_app.py", "/chatui")
